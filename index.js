07e0b360810f2903734d7eb0d2743487432ea33a
Chase Donovan <chase.donovan@aol.com>
2021-04-04 10:31:33 -0700

    stretch done

--- index.js
+++ index.js
@@ -321,7 +321,12 @@ function lotsOfArt(arr){
   return Over100;
 }
 
-
+// function lotsOfArt(arr) {
+//   return arr
+//     .filter((artist) => artist.paintings > 100)
+//     .map((artist) => artist.name);
+// }
+// console.log(lotsOfArt(artists));
 
 
 // 🎨🎨 STRETCH 🎨🎨//
@@ -346,25 +351,43 @@ The function should console.log 50 chunks of HTML code that match the structure 
 
 ‼️ You do **NOT** need to get these to display on your page, but you can copy and paste the result into your HTML file if you'd like to see what that would look like. */
 
-function getHTML(){
-    
+function getHTML(data) {
+  for (const key in data) {
+    console.log(`
+      <div id='artist'>
+        <div class='image'>
+          <img src=''/>
+        </div>
+        <div class='name'>
+          <a href='${data[key].wikipedia}'>${data[key].name}</a>
+        </div>
+        <div class='bio'>
+          ${data[key].bio}
+        </div>
+      </div>
+    `)
   }
-
+}
+getHTML(artists);
 
 /* 💪💪💪💪💪💪 STRETCH 2: 💪💪💪💪💪💪
 Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
 
-function randomize(/* Code here */){
-
-    /* Code here */
-
-  }
+function randomize(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+console.log(randomize(artists));
 
 
  /* 💪💪💪💪💪💪 STRETCH 3: 💪💪💪💪💪💪
  Use advanced array methods (.map, .reduce, .filer) to refactor your MVP code (create an array of all artists born in the 1900s with .filter, for example) */
 
- 
+//  function lotsOfArt(arr) {
+//   return arr
+//     .filter((artist) => artist.paintings > 100)
+//     .map((artist) => artist.name);
+// }
+// console.log(lotsOfArt(artists));
  
  
  /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑*/
